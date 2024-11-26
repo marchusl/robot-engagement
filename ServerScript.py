41c10d3a658144e8d2e16c5cbe08c2d0c8b5bc67
RasmusKanne <78956812+RasmusKanne@users.noreply.github.com>
2024-11-26 13:22:59 +0100

    Updating Current Head Pos
    when setting a gesture it defines the current head orientation to be able to know the current position when calling certain placement specific gestures like nodding

--- ServerScript.py
+++ ServerScript.py
@@ -12,6 +12,8 @@ message_to_send = ""
 send_message = False
 service_in_progress = False
 
+current_head_position = ""
+
 Debugging = True
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -69,11 +71,25 @@ def send(conn, msg):
     conn.send(message)
 
 def set_send_message(msg):      # Use this for Gestures (Maybe make a separate function for head rotations wherein the global currentGlobalHeadOrientation is set, so it is only updated when actually rotating/orienting the head of the robot)
-    global message_to_send, send_message, service_in_progress
+    global message_to_send, send_message, service_in_progress, current_head_position
     message_to_send = msg
     send_message = True
     service_in_progress = True
     # print(service_in_progress)
+
+    # Check message to update global current head position
+    if "[GESTURE]" in msg:
+        split_message = msg.split("-")
+        if len(split_message) == 3:
+            new_position = split_message[2]
+            current_head_position = new_position
+        elif len(split_message) <= 2:
+            new_position = "neutral"
+            current_head_position = new_position
+        else:
+            print(f"Could not set current head position: {msg}")
+
+        print(f"Current Head Position: {current_head_position}")
 
 if __name__ == "__main__":
     start_socket_streaming()
